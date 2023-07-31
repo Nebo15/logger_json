893e4dc279bf1ca71220acad84459fe86eaaf85a
Blake Kostner <git@btkostner.io>
2023-07-31 13:04:38 -0600

    feat: add request headers and params to logger body

--- lib/logger_json/plug/metadata_formatters/datadog_logger.ex
+++ lib/logger_json/plug/metadata_formatters/datadog_logger.ex
@@ -9,6 +9,17 @@ if Code.ensure_loaded?(Plug) do
     """
     import Jason.Helpers, only: [json_map: 1]
 
+    @scrubbed_keys [
+      "authentication",
+      "authorization",
+      "confirmPassword",
+      "cookie",
+      "passwd",
+      "password",
+      "secret"
+    ]
+    @scrubbed_value "*********"
+
     @doc false
     def build_metadata(conn, latency, client_version_header) do
       client_metadata(conn, client_version_header) ++
@@ -21,7 +32,9 @@ if Code.ensure_loaded?(Plug) do
               status_code: conn.status,
               method: conn.method,
               referer: LoggerJSON.PlugUtils.get_header(conn, "referer"),
+              request_headers: recursive_scrub(conn.req_headers),
               request_id: Keyword.get(Logger.metadata(), :request_id),
+              request_params: recursive_scrub(conn.params),
               useragent: LoggerJSON.PlugUtils.get_header(conn, "user-agent"),
               url_details:
                 json_map(
@@ -73,5 +86,26 @@ if Code.ensure_loaded?(Plug) do
     end
 
     defp phoenix_route(_conn), do: nil
+
+    defp recursive_scrub(%{__struct__: Plug.Conn.Unfetched}),
+      do: "%Plug.Conn.Unfetched{}"
+
+    defp recursive_scrub(data) when is_list(data) and length(data) > 100,
+      do: "List of #{length(data)} items"
+
+    defp recursive_scrub(data) when is_list(data),
+      do: Enum.map(data, &recursive_scrub/1)
+
+    defp recursive_scrub(data) when is_struct(data),
+      do: data |> Map.from_struct() |> recursive_scrub()
+
+    defp recursive_scrub(data) when is_map(data) do
+      Enum.map(data, fn
+        {k, v} when v in @scrubbed_keys -> {k, @scrubbed_value}
+        {k, v} -> {k, recursive_scrub(v)}
+      end)
+    end
+
+    defp recursive_scrub(data), do: data
   end
 end
