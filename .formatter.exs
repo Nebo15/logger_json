be7a775fe098e51f09a5c2b046b9afc77d019aaf
stord-engineering-account <77021775+stord-engineering-account@users.noreply.github.com>
2023-07-31 14:41:37 -0600

    chore: sync files with stordco/common-config-elixir (#2)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,7 @@
 # This file is synced with stordco/common-config-elixir. Any changes will be overwritten.
 
 [
-  import_deps: [:stream_data],
+  import_deps: [:ecto, :phoenix, :stream_data],
   inputs: ["*.{ex,exs}", "{config,lib,priv,test}/**/*.{ex,exs}"],
   line_length: 120,
   plugins: []
