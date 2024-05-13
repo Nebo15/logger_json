aab82262419616bd876144bca9e435fa1447c7b6
Andrew Dryga <andrew@dryga.com>
2024-05-13 15:26:07 -0600

    Fix docs for internal test struct

--- test/support/name_struct.ex
+++ test/support/name_struct.ex
@@ -1,11 +1,11 @@
 defmodule NameStruct do
   @moduledoc """
-  Needed for tests on structs that implement the Jason.Encoder protocol.
+  This struct is required for tests on structs that implement the Jason.Encoder protocol.
+
   Defining this struct in the test module wouldn't work, since the .exs files
-  are not within the compilation folders.
+  are not compiled with the application so not protocol consolidation would happen.
   """
 
   @derive Jason.Encoder
-
   defstruct [:name]
 end
