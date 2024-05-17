cfe025886beedfceb14d36c51d7663df3481aba6
Andrew Dryga <andrew@dryga.com>
2024-05-17 11:24:27 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LoggerJSON.Mixfile do
     [
       app: :logger_json,
       version: @version,
-      elixir: "~> 1.15.1",
+      elixir: "~> 1.16 or ~> 1.15.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
